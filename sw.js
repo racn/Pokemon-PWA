a6a7fc69dd1325bb65791a4dcaed18a88928d44d
raccoon <raccoon.jpn@gmail.com>
2020-12-29 09:51:29 +0900

    Initialize project

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_DATA = "offline-data";
+const STATIC_RESOURCES = ["index.html", "app.js", "logo.jpg"];
+
+// Install the SW
+self.addEventListener('install', async (e) => {
+  console.log("SW install");
+
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_DATA);
+      return await cache.addAll(STATIC_RESOURCES);
+    })()
+  );
+  
+  self.skipWaiting();
+});
+
+// Listen for fetching request
+self.addEventListener("fetch", async (e) => {
+  console.log(`SW fetch ${e.request.url}`);
+
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_DATA);
+
+      try {
+        const networkResponse = await fetch(e.request);
+        await cache.put(e.request, networkResponse.clone());
+        return networkResponse;
+      } catch (error) {
+        const cachedResponse = await chche.match(e.request);
+        return cachedResponse;
+      }
+    })()
+  );
+});
+
+// Activate the SW
+self.addEventListener("activate", async (e) => {
+  console.log("SW activate");
+});
